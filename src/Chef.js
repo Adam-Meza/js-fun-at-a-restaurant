f4d9488b4cab5886beb089b15e5cbae40bfcfc74
Adam Meza <Adammeza95@gmail.com>
2023-02-03 16:29:21 -0500

    Fix Last Issue

--- src/Chef.js
+++ src/Chef.js
@@ -11,14 +11,12 @@ class Chef {
     return `Hello, ${name}!`
   }
 
-  checkForFood(item){
+  checkForFood(item,){
     let x = item.type
       for (let i = 0; i< this.restaurant.menus[x].length; i++){
-
-      // if (this.restaurant.menus[x][i].type.includes(item)){}
-      // deeply confused by this. it works with or without the conditional. feels like i just lucky? what it is im not understanding? it doesn't work when I try to add the Affimitive return into it. 
-
-      return `Yes, we're serving ${item.name} today!`
+      if (this.restaurant.menus[x][i] === (item)){
+        return `Yes, we're serving ${item.name} today!`
+      }
     } 
     return `Sorry, we aren't serving ${item.name} today.`
   } 
